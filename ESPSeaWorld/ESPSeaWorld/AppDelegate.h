b3c3a7c4eba3fe0bf10e2591a2839b5987d635c6
Kate <culickovacat@yandex.ru>
2017-02-08 12:47:47 +0700

    initial

---
+++ ESPSeaWorld/ESPSeaWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ESPSeaWorld
+//
+//  Created by Kate on 08.02.17.
+//  Copyright © 2017 Kate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
